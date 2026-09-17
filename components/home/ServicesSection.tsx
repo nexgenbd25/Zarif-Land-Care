246c3911a2f28d6918a37f3833934d5b912d0e9d
nexgenbd25 <mdshohelranabd585@gmail.com>
2026-09-17 13:31:25 +0600

    Update ServicesSection.tsx

--- components/home/ServicesSection.tsx
+++ components/home/ServicesSection.tsx
@@ -13,6 +13,7 @@ import {
   ArrowRight,
   LucideIcon,
 } from 'lucide-react';
+import SectionDivider from '@/components/ui/SectionDivider';
 
 interface Service {
   id: number;
@@ -107,7 +108,10 @@ export default function ServicesSection() {
   };
 
   return (
-    <section className="relative overflow-hidden section-padding bg-navy">
+    <section className="relative overflow-hidden section-padding bg-navy
+                        border-b border-navy-border">
+      <SectionDivider />
+
       <div
         className="absolute top-0 left-1/2 -translate-x-1/2 
                    w-[600px] h-[600px] bg-gold/5 rounded-full 
