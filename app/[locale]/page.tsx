910fc8cfeca5b52fc510ff84afe619e614dac529
nexgenbd25 <mdshohelranabd585@gmail.com>
2026-09-17 13:22:24 +0600

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,13 @@
 import HeroSection from '@/components/home/HeroSection';
-import ServicesSection from '@/components/home/ServicesSection';
 import AboutSection from '@/components/home/AboutSection';
+import ServicesSection from '@/components/home/ServicesSection';
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       <HeroSection />
-      <ServicesSection />
       <AboutSection />
+      <ServicesSection />
     </main>
   );
 }
