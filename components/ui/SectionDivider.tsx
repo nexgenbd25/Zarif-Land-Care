dc4726968c0ab74ce4d6f4f524524e4efe5e1add
nexgenbd25 <mdshohelranabd585@gmail.com>
2026-09-17 13:29:35 +0600

    Create SectionDivider.tsx

---
+++ components/ui/SectionDivider.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function SectionDivider() {
+  return (
+    <div className="relative h-px w-full">
+      <div className="absolute inset-0 bg-gradient-to-r from-transparent via-gold/30 to-transparent" />
+    </div>
+  );
+}
